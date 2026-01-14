33cc3e82ad3796e2b02ccc9f070e909e4c4d3f51
Alex-Brosnan-1 <126abrosnan@frhsd.com>
2026-01-14 08:04:05 -0500

    Setup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser"); //Allows json data in body section
 
 const app = express();
 const port = 3000;
-const message = "hello world";
+const message = ["test"];
 
 app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'] // Specify allowed methods
@@ -12,11 +12,25 @@ app.use(cors({
 app.use(bodyParser.json());
 
 // Define a GET route at the root URL ('/')
-app.get('/', (req, res) => {
+app.get('/tester', (req, res) => {
 
     // Use res.json() to send a JSON response
     // It automatically converts the provided object/variable into JSON
     res.json({
         data: message
     });
+});
+
+app.post('/tester', (req, res) => {
+    const { data } = req.body;
+    message.push(data);
+
+    res.json({
+        success: true,
+        data: message
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
